238a6db8802ca6274d78d91b4a673543bbfde1be
Junaid Siddiqui <maxeinstein.dev@gmail.com>
2022-07-22 17:10:26 +0530

    Added docker-compose.yml  to run django app and postgres db as docker containers

--- acm_portal/settings.py
+++ acm_portal/settings.py
@@ -84,8 +84,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
